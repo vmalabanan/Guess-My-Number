0ebb1f3702c9d64488c88fa26a05d59be76a6452
vmalabanan <63671843+vmalabanan@users.noreply.github.com>
2022-05-18 00:42:08 -0700

    Refactor code

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number! 🥳';
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = '??';
-document.querySelector('.score').textContent = '20,000,000';
-document.querySelector('.guess').value = 85;
-console.log(document.querySelector('.guess').value);
-*/
 
 // Function to generate a random number between 1 and 20
 const randomNumber = () => Math.trunc(Math.random() * 20) + 1;
@@ -17,6 +6,10 @@ const randomNumber = () => Math.trunc(Math.random() * 20) + 1;
 // Function to set text content of an element
 const setTextContent = (element, text) =>
   (document.querySelector(element).textContent = text);
+
+// Function to display message (set text content of message element)
+const displayMessage = message =>
+  (document.querySelector('.message').textContent = message);
 
 // Function to set value of an element
 const setValue = (element, value) =>
@@ -29,18 +22,6 @@ const setBackgroundColor = (element, value) =>
 // Function to set width of an element
 const setWidth = (element, value) =>
   (document.querySelector(element).style.width = value);
-
-// // Function to run when game is won/lost
-// const endGame = condition => {
-//   // Set condition = true;
-//   condition = true;
-
-//   // Display win/lose message
-//   if (condition === win) setTextContent('.message', winMessage);
-
-//   // Display hidden number
-//   setTextContent('.number', num);
-// };
 
 // Declaring variables
 let num = randomNumber();
@@ -69,28 +50,54 @@ document.querySelector('.check').addEventListener('click', function () {
   // Declare/initialize guess variable
   const guess = Number(document.querySelector('.guess').value);
 
-  // If player has already won or lost the game, do nothing
+  // If player has already won or lost the game, return
   if (win || lose) return;
-
   // If guess < 1 or > 20, display error message and return
-  if (guess < 1 || guess > 20) {
-    setTextContent('.message', notInRangeMessage);
+  else if (guess < 1 || guess > 20) {
+    displayMessage(notInRangeMessage);
     return;
   }
 
   // If there is no input in the text box, display error message and return
-  if (!guess) {
-    setTextContent('.message', noNumberMessage);
+  else if (!guess) {
+    displayMessage(noNumberMessage);
     return;
   }
 
+  // If guess is incorrect
+  else if (guess !== num) {
+    // Decrement score variable
+    setTextContent('.score', --score);
+
+    // If score reaches 0
+    if (score <= 0) {
+      // Set lose = true
+      lose = true;
+
+      // Display lose message
+      displayMessage(loseMessage);
+
+      // Display hidden number
+      setTextContent('.number', num);
+
+      // Expand width of the number box
+      // document.querySelector('.number').style.width = '30rem';
+      setWidth('.number', '30rem');
+
+      return;
+    }
+
+    // If guess is too high, display too high message. Else display too low message.
+    displayMessage(guess > num ? tooHighMessage : tooLowMessage);
+  }
+
   // If guess is correct
-  else if (guess === num) {
+  else {
     // Set win = true;
     win = true;
 
     // Display win message
-    setTextContent('.message', winMessage);
+    displayMessage(winMessage);
 
     // Display hidden number
     setTextContent('.number', num);
@@ -108,32 +115,6 @@ document.querySelector('.check').addEventListener('click', function () {
     // document.querySelector('.number').style.width = '30rem';
     setWidth('.number', '30rem');
   }
-
-  // Decrement score variable
-  setTextContent('.score', --score);
-
-  // If score reaches 0
-  if (score <= 0) {
-    // Set lose = true
-    lose = true;
-
-    // Display lose message
-    setTextContent('.message', loseMessage);
-
-    // Display hidden number
-    setTextContent('.number', num);
-
-    // Expand width of the number box
-    // document.querySelector('.number').style.width = '30rem';
-    setWidth('.number', '30rem');
-
-    return;
-  }
-
-  // If guess is too high
-  if (guess > num) setTextContent('.message', tooHighMessage);
-  // If guess is too low
-  else if (guess < num) setTextContent('.message', tooLowMessage);
 });
 
 // Again button
@@ -148,10 +129,10 @@ document.querySelector('.again').addEventListener('click', function () {
   // Generate a new random number
   num = randomNumber();
 
-  console.log(num); // Remove -- for testing only!
+  // console.log(num); // Remove -- for testing only!
 
   // Reset message to starting message
-  setTextContent('.message', startMessage);
+  displayMessage(startMessage);
 
   // Reset score to starting score
   score = setTextContent('.score', startScore);
